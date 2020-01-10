661dde9d85ee1a158f8b8e16e7763c1812d81f39
dane <danielleespiritu17@gmail.com>
2020-01-10 21:46:02 +0800

    2

--- thesis/mainApp/views.py
+++ thesis/mainApp/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import devicestatus;
+from .models import devicestatus
 
 import sys
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(21, GPIO.OUT, initial=0) #Fans
-GPIO.setup(20, GPIO.OUT, initial=0) #Lights
-GPIO.setup(16, GPIO.OUT, initial=0) #Seeder
-GPIO.setup(12, GPIO.OUT, initial=0) #Water
+GPIO.setup(21, GPIO.OUT, initial=0)  # Fans
+GPIO.setup(20, GPIO.OUT, initial=0)  # Lights
+GPIO.setup(16, GPIO.OUT, initial=0)  # Seeder
+GPIO.setup(12, GPIO.OUT, initial=0)  # Water
+
 
 def mainPage(response):
     deviceStatusObjects = devicestatus.objects.latest('date')
@@ -56,16 +57,56 @@ def mainPage(response):
         insertDeviceStatus.seedStatus = deviceStatusObjects.seedStatus
         insertDeviceStatus.save()
 
+    if response.POST.get('action') == 'onWater':
+
+        GPIO.output(16, GPIO.HIGH)
+
+        label = response.POST.get('label')
+        insertDeviceStatus.fansStatus = deviceStatusObjects.fansStatus
+        insertDeviceStatus.lightsStatus = deviceStatusObjects.lightsStatus
+        insertDeviceStatus.waterStatus = 'on'
+        insertDeviceStatus.seedStatus = deviceStatusObjects.seedStatus
+        insertDeviceStatus.save()
+
+    if response.POST.get('action') == 'offWater':
+
+        GPIO.output(16, GPIO.LOW)
+
+        label = response.POST.get('label')
+        insertDeviceStatus.fansStatus = deviceStatusObjects.fansStatus
+        insertDeviceStatus.lightsStatus = deviceStatusObjects.lightsStatus
+        insertDeviceStatus.waterStatus = 'off'
+        insertDeviceStatus.seedStatus = deviceStatusObjects.seedStatus
+        insertDeviceStatus.save()
+
+    if response.POST.get('action') == 'onSeed':
+
+        GPIO.output(12, GPIO.HIGH)
+
+        insertDeviceStatus.fansStatus = deviceStatusObjects.fansStatus
+        insertDeviceStatus.lightsStatus = deviceStatusObjects.lightsStatus
+        insertDeviceStatus.waterStatus = deviceStatusObjects.waterStatus
+        insertDeviceStatus.seedStatus = 'on'
+        insertDeviceStatus.save()
+
+    if response.POST.get('action') == 'offSeed':
+
+        GPIO.output(12, GPIO.LOW)
+
+        insertDeviceStatus.fansStatus = deviceStatusObjects.fansStatus
+        insertDeviceStatus.lightsStatus = deviceStatusObjects.lightsStatus
+        insertDeviceStatus.waterStatus = deviceStatusObjects.waterStatus
+        insertDeviceStatus.seedStatus = 'off'
+        insertDeviceStatus.save()
 
     return render(response, 'main.html', {'deviceStatusObjects': deviceStatusObjects})
+
 
 def databasePage(response):
 
     deviceStatusObjects = devicestatus.objects.all()
 
     return render(response, 'database.html', {'deviceStatusObjects': deviceStatusObjects})
-
-
 
 
 # Create your views here.
