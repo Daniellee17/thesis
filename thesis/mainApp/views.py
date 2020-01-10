c80392dec29aeda9614193ea8886c5a14b17994c
dane <danielleespiritu17@gmail.com>
2020-01-10 22:36:38 +0800

    sesnsors

--- thesis/mainApp/views.py
+++ thesis/mainApp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import devicestatus
+from .models import sensors
+from pygame.locals import *
 
 import sys
+import pygame
+import pygame.camera
+import Adafruit_DHT
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
 
+sensor = Adafruit_DHT.DHT11
+
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT, initial=0)  # Fans
 GPIO.setup(20, GPIO.OUT, initial=0)  # Lights
 GPIO.setup(16, GPIO.OUT, initial=0)  # Seeder
@@ -13,8 +20,24 @@ GPIO.setup(12, GPIO.OUT, initial=0)  # Water
 
 
 def mainPage(response):
+
+    print("------------------------------------------REFRESHED!------------------------------------------")
+
     deviceStatusObjects = devicestatus.objects.latest('date')
+    sensorsObjects = sensors.objects.latest('date')
+
+    # Create instance para makapag insert
     insertDeviceStatus = devicestatus()
+    insertSensors = sensors()
+
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, 1)
+
+    currentTemperature = temperature
+    currentHumidity = humidity
+    #currentMoisture = sensorsObjects.humidity
+    #currentSummary = sensorsObjects.summary
+    currentMoisture = '100'
+    currentSummary = 'default'
 
     if response.POST.get('action') == 'onFan':
 
@@ -98,6 +121,55 @@ def mainPage(response):
         insertDeviceStatus.waterStatus = deviceStatusObjects.waterStatus
         insertDeviceStatus.seedStatus = 'off'
         insertDeviceStatus.save()
+
+    if(currentTemp > 30):
+
+        # Turn on fans automatically
+        GPIO.output(21, GPIO.HIGH)
+        insertDeviceStatus.fansStatus = 'on'
+        insertSensors.temperature = currentTemperature
+        insertSensors.humidity = currentHumidity
+        insertSensors.moisture = currentMoisture
+
+        if(currentHumidity < 40):
+            insertSensors.summary = 'Temperature is too high and Humidity is too low!!!'
+            save()
+        else:
+            insertSensors.summary = 'Temperature is too high!!!'
+            save()
+
+    if(currentHumidity < 40):
+
+        insertSensors.temperature = currentTemperature
+        insertSensors.humidity = currentHumidity
+        insertSensors.moisture = currentMoisture
+
+        if(currentTemp>30):
+            insertSensors.summary = 'Temperature is too high and Humidity is too low!!!'
+            save()
+        else:
+            insertSensors.summary = 'Humidity is too low!!!'
+            save()
+
+    if(currentTemp < 30):
+
+        # Turn off fans automatically
+        GPIO.output(21, GPIO.LOW)
+        insertDeviceStatus.fansStatus = 'off'
+        insertSensors.temperature = currentTemperature
+        insertSensors.humidity = currentHumidity
+        insertSensors.moisture = currentMoisture
+
+        if(currentHumidity > 40):
+            insertSensors.summary = 'Temperature and Humidity are okay!!!'
+            save()
+        else:
+            insertSensors.summary = 'Humidity is too low!!!'
+            save()
+
+
+
+
 
     return render(response, 'main.html', {'deviceStatusObjects': deviceStatusObjects})
 
