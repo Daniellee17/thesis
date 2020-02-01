d6b8867b410a20d14077e12ad3c4db2cb7d219f3
Daniellee17 <danielleespiritu17@gmail.com>
2020-02-01 03:20:27 -0500

    addedPLantDB

--- thesis/mainApp/models.py
+++ thesis/mainApp/models.py
@@ -20,5 +20,15 @@ class devicestatus(models.Model):
 
 class camerasnaps(models.Model):
     camera = models.ImageField(default='default.png', blank=True);
-    cameraURL = models.ImageField(default='../assets/gardenPics/default.png', blank=True); 
+    cameraURL = models.ImageField(default='../assets/gardenPics/default.png', blank=True);
+    plant1 = models.IntegerField(default=0);
+    plant2 = models.IntegerField(default=0);
+    plant3 = models.IntegerField(default=0);
+    plant4 = models.IntegerField(default=0);
+    plant5 = models.IntegerField(default=0);
+    plant6 = models.IntegerField(default=0);
+    plant7 = models.IntegerField(default=0);
+    plant8 = models.IntegerField(default=0);
+    plant9 = models.IntegerField(default=0);
+    plant10 = models.IntegerField(default=0);
     date = models.DateTimeField(auto_now=True);
